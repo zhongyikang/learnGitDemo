635f2e386e865ff0dc2a9f1f64649f93a7c86c08
zhongyikang <2472082117@qq.com>
2021-09-30 23:40:13 +0800

    修改user类

--- src/main/java/com/example/demo/pojo/User.java
+++ src/main/java/com/example/demo/pojo/User.java
@@ -1,11 +1,15 @@
 package com.example.demo.pojo;
 
+import lombok.Data;
+
 /**
  * @author zhongyikang
  * @create 2021-09-30 23:30
  */
+@Data
 public class User {
 
-    private String name;
+    private String id;
+    private String nickname;
     private Integer age;
 }
